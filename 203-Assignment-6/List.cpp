250c7f4eb9a901aa1c9d17cec619fe05d3cfb31c
rhinds1004 <rhinds1004@gmail.com>
2015-05-10 21:04:23 -0700

    commit 4. just added a few comments.

--- 203-Assignment-6/List.cpp
+++ 203-Assignment-6/List.cpp
@@ -20,10 +20,14 @@ void List::setHeadNode(Node* aNode)
 {
 	mHeadNode = aNode;
 }
+
+//Records the address of the Node dynamically allocated in the constructor.
+//to allow deleting.
 void List::setNewNode(Node* aNode)
 {
 	mNewNode = aNode;
 }
+//Function to check if dynamically allocated Node needs to be deleted.?
 List::~List()
 {
 	delete mNewNode;
